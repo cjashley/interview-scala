c75de1cd04d0bdbffdf5570e53c0ca94f1dffa76
cjashley <chris.ashley@fast-email.com>
2022-09-02 22:18:57 +0900

    QueryParams attempt at getting Invalid Currency returned, bt cann foind a way to attach handeler into http4s

--- forex-mtl/src/main/scala/forex/http/rates/QueryParams.scala
+++ forex-mtl/src/main/scala/forex/http/rates/QueryParams.scala
@@ -1,13 +1,26 @@
 package forex.http.rates
 
+import cats.data.{Validated, ValidatedNel}
 import forex.domain.Currency
-import org.http4s.QueryParamDecoder
 import org.http4s.dsl.impl.QueryParamDecoderMatcher
+import org.http4s.{ParseFailure, QueryParamDecoder, QueryParameterValue}
 
 object QueryParams {
 
   private[http] implicit val currencyQueryParam: QueryParamDecoder[Currency] =
-    QueryParamDecoder[String].map(Currency.fromString)
+    new QueryParamDecoder[Currency]  {
+      // https://stackoverflow.com/questions/67710359/decode-an-optional-query-parameter-using-queryparamdecoder-in-scala
+      def decode(part: QueryParameterValue): ValidatedNel[ParseFailure, Currency] = {
+        //           throw new ParseFailure("Invalid Currency","currency is unknown "+part)
+        Validated.catchOnly[MatchError] {
+          println("decode "+part.value)
+          val c = Currency.fromString(part.value)
+          println("decoded "+c)
+          (c)
+        }.leftMap(e => ParseFailure("Invalid query parameter part", e.getMessage))
+          .toValidatedNel
+      }
+    }
 
   object FromQueryParam extends QueryParamDecoderMatcher[Currency]("from")
   object ToQueryParam extends QueryParamDecoderMatcher[Currency]("to")
--- forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
+++ forex-mtl/src/main/scala/forex/services/rates/interpreters/OneFrameDummy.scala
@@ -10,7 +10,10 @@ import forex.services.rates.errors._
 class OneFrameDummy[F[_]: Applicative] extends Algebra[F] {
 
   override def getRate(pair: Rate.Pair): F[Error Either Rate] =
-    Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F] // TODO get rate via OneFrame
+    {
+
+      Rate(pair, Price(BigDecimal(100)), Timestamp.now).asRight[Error].pure[F] // TODO get rate via OneFrame
+    }
 
   override def getCurrencies(): F[Either[Error, Currencies]] =
     Currencies(Currency.all).asRight[Error].pure[F] // return our limited currencies list (OneFrame can accept many more pairs)
